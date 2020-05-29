cd729e5c37382866d28079c6b81a8258cbe8d042
TheSoulsHunter <konrad.romanski4@gmail.com>
2020-05-29 19:43:14 +0200

    more code added

--- flashcards.sh
+++ flashcards.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+
+SEP=';'
 for i in "$@"; do
-  echo $i
+  case $i in
+    -h);&
+    --help)
+    echo "Welcome to help!"
+    echo "Usage: ./flashcards [OPTION]... [FILE]"
+    echo
+    echo "  -h, --help       to show this informations"
+    echo "  -s, --separator  to change default (';') separator"
+    ;;
+
+    -s);&
+    --separator)
+      shift
+      SEP=$i
+    ;;
+  esac
 done
+echo $SEP
