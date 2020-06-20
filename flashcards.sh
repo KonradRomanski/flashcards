f8a3585eb6be3c245890ce2d8d1b4c0647790ddb
Konrad <konrad.romanski4@gmail.com>
2020-06-20 22:56:03 +0200

    Basic functionality added

--- flashcards.sh
+++ flashcards.sh
@@ -5,6 +5,7 @@ MODE='F'
 READ='F'
 CAMO='F'
 HELP='F'
+WP='F'
 FILES=[]
 items=0
 shi=0
@@ -60,34 +61,67 @@ for i in "$@"; do
     CAMO='T'
     ;;
 
+    -?*)
+    shift
+    WP='T'
+    ;;
+
     *)
     if [[ $shi == 0 ]]; then
       FILES[items]=$1
-      echo "*" $items ${FILES[items]} $i
+      # echo "*" $items ${FILES[items]} $i
       shift
       items=$(($items + 1))
     else
-      SHI=$(($SHI - 1))
+      shi=$(($shi - 1))
       shift
     fi
     ;;
 
   esac
-  echo === -$1  $i
+  # echo === -$1  $i
 done
-
-if [[ $HELP == 'F' && ${#FILES[*]} == 1 ]]; then
+if [[ $WP == 'T' ]]; then
+  echo "Wrong parameter.Try again or use help flag ('-h'/'--help') for more information."
+elif [[ $HELP == 'F' && ${FILES[@]} == '[]' ]]; then
   echo "No data prvided. Try again or use help flag ('-h'/'--help') for more information."
-# else
+else
+  FOREIGN=($(cat ${FILES[@]} | cut -d $SEP -f1 | tr '\n' ' ' | rev | cut -c2- | rev))
+  NATIVE=($(cat ${FILES[@]} | cut -d $SEP -f2 | tr '\n' ' ' | rev | cut -c2- | rev))
+  # echo
+  echo ${FOREIGN[*]}
+  # echo ${NATIVE[*]}
+ echo ${#FOREIGN[@]}
 
+  while [[ $ff < ${#FOREIGN[@]} ]] ; do
+
+    echo -n "${NATIVE[$ff]} - "
+
+    read -r temp
+    if [[ $temp == ${FOREIGN[$ff]} ]] ; then
+      SCORE=$(($SCORE + 1))
+    else
+      FAILS=$(($FAILS + 1))
+    fi
+
+    # echo $ff
+    ff=$(($ff + 1))
+
+  done
 fi
 
-echo "SCRIPT TESTING:"
-echo "---------------"
-echo "Current SEP:    $SEP"
-echo "Current MODE:   $MODE"
-echo "Current READ:   $READ"
-echo "Current CAMO:   $CAMO"
-echo "Current items:  $items"
-echo "Current HELP:   $HELP"
-echo "Current FILES:  ${FILES[*]} ${#FILES[*]}"
+  echo "Your score: $SCORE"
+  echo "Your fails: $FAILS"
+  # echo "Percentage of correct answears: " $(( $(( $SCORE / $(( $SCORE + $FAILS )) )) * 100 ))
+  # echo "Percentage of correct answears: "   $(echo "scale=4;1/$FAILS" | bc )
+  echo "Percentage of correct answears: " $(echo "scale=2;($SCORE / ($SCORE + $FAILS)) * 100" | bc ) "%"
+
+# echo "SCRIPT TESTING:"
+# echo "---------------"
+# echo "Current SEP:    $SEP"
+# echo "Current MODE:   $MODE"
+# echo "Current READ:   $READ"
+# echo "Current CAMO:   $CAMO"
+# echo "Current items:  $items"
+# echo "Current HELP:   $HELP"
+# echo "Current FILES:  ${FILES[*]} ${#FILES[*]}"
